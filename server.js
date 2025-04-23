9ea6c7739414f7456b547aece5a5c94bada07ac0
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-04-23 12:27:57 +0700

    create http server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Haloo, kita akan belajar membuat server");
+const http = require("http");
+
+const requestListiner = (request, response) => {
+  response.setHeader("Content-Type", "text/html");
+
+  response.statusCode = 200;
+  response.end("<h1>Hallo HTTP Server</h1>");
+};
+
+const server = http.createServer(requestListiner);
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+  console.log(`Server is running at http://${host}:${port}`);
+});
